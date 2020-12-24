6c09f3b59d425748ef7d87985468f3b33e9513bb
unknown <crregula@gmail.com>
2020-12-24 00:01:18 -0500

    [world-cities]: added apiResult class

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -22,10 +22,18 @@ namespace WorldCities.Controllers
         }
 
         // GET: api/Cities
+        // GET: api/Cities/?pageIndex=0&pageSize=10
+        // GET: api/Cities/0/10
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<City>>> GetCities()
+        [Route("{pageIndex?}/{pageSize?}")]
+        public async Task<ActionResult<ApiResult<City>>> GetCities(
+            int pageIndex = 0,
+            int pageSize = 10)
         {
-            return await _context.Cities.ToListAsync();
+            return await ApiResult<City>.CreateAsync(
+                _context.Cities,
+                pageIndex,
+                pageSize);
         }
 
         // GET: api/Cities/5
---
+++ Data/ApiResult.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data
+{
+    public class ApiResult<T>
+    {
+        #region Properties
+        /// <summary>
+        /// The data result
+        /// </summary>
+        public List<T> Data { get; private set; }
+
+        /// <summary>
+        /// Zero-based index of current page.
+        /// </summary>
+        public int PageIndex { get; private set; }
+
+        /// <summary>
+        /// Number of items contained in each page.
+        /// </summary>
+        public int PageSize { get; private set; }
+
+        /// <summary>
+        /// Total items count
+        /// </summary>
+        public int TotalCount { get; set; }
+
+        /// <summary>
+        /// Total pages count
+        /// </summary>
+        public int TotalPages { get; set; }
+
+        /// <summary>
+        /// TRUE if the current page has a previous page,
+        /// FALSE otherwise.
+        /// </summary>
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 0);
+            }
+        }
+
+        /// <summary>
+        /// TRUE if the current page has a next page,
+        /// FALSE otherwise.
+        /// </summary>
+        public bool HasNextPage { 
+            get 
+            {
+                return ((PageIndex + 1) < TotalPages);
+            } 
+        }
+        #endregion
+
+        /// <summary>
+        /// Private constructor called by the CreateAsync method.
+        /// </summary>
+        private ApiResult(List<T> data, int count, int pageIndex, int pageSize)
+        {
+            Data = data;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalCount = count;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+
+        #region Methods
+        public static async Task<ApiResult<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            source = source
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize);
+
+            var data = await source.ToListAsync();
+
+            return new ApiResult<T>(
+                data,
+                count,
+                pageIndex,
+                pageSize);
+        }
+        #endregion
+    }
+}
