4d7b08799db882442fe2a83dcf739eb7690f130a
unknown <crregula@gmail.com>
2020-12-24 08:28:22 -0500

    updated the apiresult to include sorting props

--- Data/ApiResult.cs
+++ Data/ApiResult.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
 
 namespace WorldCities.Data
 {
@@ -56,24 +58,42 @@ namespace WorldCities.Data
                 return ((PageIndex + 1) < TotalPages);
             } 
         }
+
+        /// <summary>
+        /// Sorting column name (or null if not set)
+        /// </summary>
+        public string SortColumn { get; set; }
+
+        /// <summary>
+        /// Sorting Order ("ASC", "DESC" or null if not set)
+        /// </summary>
+        public string SortOrder { get; set; }
         #endregion
 
         /// <summary>
         /// Private constructor called by the CreateAsync method.
         /// </summary>
-        private ApiResult(List<T> data, int count, int pageIndex, int pageSize)
+        private ApiResult(List<T> data, int count, int pageIndex, int pageSize, string sortColumn, string sortOrder)
         {
             Data = data;
             PageIndex = pageIndex;
             PageSize = pageSize;
             TotalCount = count;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            SortColumn = sortColumn;
+            SortOrder = sortOrder;
         }
 
         #region Methods
-        public static async Task<ApiResult<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        public static async Task<ApiResult<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize, string sortColumn = null, string sortOrder = null)
         {
             var count = await source.CountAsync();
+
+            if (!string.IsNullOrEmpty(sortColumn) && IsValidProperty(sortColumn))
+            {
+                sortOrder = !string.IsNullOrEmpty(sortOrder) && sortOrder.ToUpper() == "ASC" ? "ASC" : "DESC";
+                source = source.OrderBy(string.Format("{0} {1}", sortColumn, sortOrder));
+            }
             source = source
                 .Skip(pageIndex * pageSize)
                 .Take(pageSize);
@@ -84,7 +104,20 @@ namespace WorldCities.Data
                 data,
                 count,
                 pageIndex,
-                pageSize);
+                pageSize,
+                sortColumn,
+                sortOrder);
+        }
+
+        public static bool IsValidProperty(string propertyName, bool throwExceptionIfNotFound = true)
+        {
+            var prop = typeof(T).GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            if (prop == null && throwExceptionIfNotFound)
+            {
+                throw new NotSupportedException(string.Format("ERROR: Property '{0}' does not exist.", propertyName));
+            }
+
+            return prop != null;
         }
         #endregion
     }
